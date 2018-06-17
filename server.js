b112a668d5b7aef71c8fc9ba3fb6a5416159c590
unknown <jtc7537@gmail.com>
2018-06-17 19:05:27 -0400

    basic server

--- server.js
+++ server.js
@@ -16,4 +16,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
+const html = require('./controllers/html')
+html(app);
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
