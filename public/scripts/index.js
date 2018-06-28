863d8d5ae877cae2c125263ff4e369e673b95ab6
unknown <jtc7537@gmail.com>
2018-06-27 23:39:02 -0400

    tiles

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,12 +2,21 @@ $.get('/recipes', data => {
     console.log(data)
     data.forEach((e,i) => {
         console.log(e)
+        console.log(i)
         const rec = `
-        <div class='recipe-block'>
-            <a href='/recipes/${e._id}'><div class='title' id='rec-${i}'>${e.title}</div></a>
-            <img src='${e.images[0]}'>
+        <div style='background-image:url("${e.images[0]}")' class='recipe-block'>
+            <a id='rec-${i}' class='title' href='/recipes/${e._id}'><div>${e.title}</div></a>
+            <div class='tag-cont' id='tags-${i}'></div>
         </div>`
-        $('body').append(rec)    
+        $('body').append(rec)
+        e.tags.forEach(g => {
+            console.log(g);
+            console.log(i)
+            console.log(`#rec-${i}`)
+            const tag = `
+            <div class='tag'>${g}</div>`
+            $(`#tags-${i}`).append(tag)
+        })    
     })
         
 })
