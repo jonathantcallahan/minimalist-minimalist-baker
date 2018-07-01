810c02219f6b85f37b3fccac157b0ef7f23df214
unknown <jtc7537@gmail.com>
2018-06-30 21:17:03 -0400

    details

--- public/scripts/recipe.js
+++ public/scripts/recipe.js
@@ -9,6 +9,13 @@ $( document ).ready(function(){
         console.log(data)
         images = data.images
         $('#food-img').attr('src',data.images[0])
+        $('#title').text(data.title);
+        $('#ingredients').html(data.ingredients)
+        $('.wprm-unit-conversion-container').remove()
+        $('#instructions').html(data.instructions)
+        $('#time').append(data.time)
+        $('#fridge').append(data.fridge)
+        $('#freezer').append(data.freezer)
     })
 
     $('.scroll').click(function(){
@@ -17,13 +24,14 @@ $( document ).ready(function(){
         const food = $('#food-img');
         const left = $('#left');
         const right = $('#right');
+
         dir === 'right' ? currentImg++ : currentImg-- ;
         food.attr('src',images[currentImg]);
         currentImg > 0 ? left.css('visibility','visible') : left.css('visibility','hidden')
         currentImg === imgLen -1 ? right.css('visibility','hidden') : right.css('visibility','visible');
 
         $(this).css('width','40px').css('height','40px')
-        setTimeout(() => { $(this).css('width','30px').css('height','30px')},350)
+        setTimeout(() => { $(this).css('width','30px').css('height','30px')},200)
     })
 })
 
